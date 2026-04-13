7badd9c9c39061d77c6ebcaf47e7ff1e0409e61c
adhvaidha <adhvaidha.h2023@vitstudent.ac.in>
2026-04-13 10:17:04 +0530

    initial commit

---
+++ src/test/java/LibraryTest.java
@@ -0,0 +1,32 @@
+import static org.junit.jupiter.api.Assertions.*;
+import org.junit.jupiter.api.Test;
+
+public class LibraryTest {
+
+    @Test
+    void testAddBooks() {
+        Library lib = new Library();
+        lib.addBook(10);
+        assertEquals(10, lib.getAvailableBooks());
+    }
+
+    @Test
+    void testLendBooksNormal() {
+        Library lib = new Library();
+        lib.addBook(10);
+        assertEquals(5, lib.lendBook(5));
+    }
+
+    @Test
+    void testLendMoreThanAvailable() {
+        Library lib = new Library();
+        lib.addBook(5);
+        assertEquals(-1, lib.lendBook(10));
+    }
+
+    @Test
+    void testZeroBooks() {
+        Library lib = new Library();
+        assertEquals(0, lib.getAvailableBooks());
+    }
+}
